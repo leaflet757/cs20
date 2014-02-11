958eb37e9c71ea2abfc9f76d9f48a220eabf23c3
leaflet757 <chatsboy@aol.com>
2014-02-10 20:22:41 -0800

    Merge branch 'master' into weaponsbranch

--- main.js
+++ main.js
@@ -241,7 +241,12 @@ function initScene(){
 	
 	// graphics.addToDisplay(testMap,"gl_main")
 	currentMap = new Map(9,0.5,256*2,512*2,256*2,512*2,640*2,128);
-	currentMap.init();
+	Entities.runner.def.max = 20;
+	currentMap.init([Entities.runner],32);
+	// Entities.runner.newInstance(Math.random()*500, Math.random()*500);
+	// Entities.runner.newInstance(Math.random()*500, Math.random()*500);
+	// Entities.runner.newInstance(Math.random()*500, Math.random()*500);
+	// Entities.runner.newInstance(Math.random()*500, Math.random()*500);
 	// Entities.player.newInstance(currentScreen.width/2,currentScreen.height/2);
 	Entities.runner.newInstance(Math.random()*500, Math.random()*500);
 	Entities.runner.newInstance(Math.random()*500, Math.random()*500);
@@ -249,6 +254,9 @@ function initScene(){
 	Entities.runner.newInstance(Math.random()*500, Math.random()*500);
 	physics.setGeometry(currentMap.lines);
 	graphics.addToDisplay(currentMap,'gl_main');
+	Sound.addBuffer('test','resources/audio/laser4.wav')
+	testSound = Sound.createSound('test',true);
+	testSound2 = Sound.createSound('test',true);
 }
 
 function reinitScene(){
